093c36451463fc2898e7fe19259ef7560704c683
alanaqueirozb@gmail.com <alanaqueirozb@gmail.com>
2024-10-25 13:38:42 -0300

    Continuação dos exercícios

---
+++ CSharpOOP5/CSharpOOP5/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+
+namespace CSharpOOP5 //Exercício 1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero1, numero2, soma;
+
+            Console.WriteLine("Escreva o primeiro número: ");
+            numero1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Escreva o segundo número: ");
+            numero2 = Convert.ToInt32(Console.ReadLine());
+
+            soma = numero1 + numero2;
+
+            Console.WriteLine($"Soma = {soma}");
+        }
+    }
+}
